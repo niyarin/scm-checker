30f26dcbcb0121f6ac04caa3a351d4b4135e1777
niyarin <inoanet@gmail.com>
2025-07-30 09:50:23 +0900

    feat: Update test-resources.

--- test-resources/source1.scm
+++ test-resources/source1.scm
@@ -3,8 +3,20 @@
         (only (scheme base) cons)
         (scheme read))
 
-(cond
-  ((eq? x 'aaa) #t)
-  ((eq? x 'bbb) #f)
-  (else #f))
+(and (cons 1 2)
+     (and (cons 3 4) (cons 100 200))
+     (cons 5 6))
 
+
+(if test foo #f)
+
+(if #t foo 3)
+(if #f foo 3)
+
+(cond
+  ((zero? x) #f)
+  ((= x 3) #t)
+  ((eq? x (quote a)) #t)
+  (else "HELLO"))
+
+(and 100000)
